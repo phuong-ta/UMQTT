9976a04e17ba7c745923cd42aececdb1e2c20161
phuong.ta <phuong.ta011290@gmail.com>
2023-02-13 20:25:53 +0200

    main file done

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import time
-from umqtt.simple import MQTTClient
 from MqttConnection import listenBroker
-from WifiConnection import connectWifi
+
 def main():
-    
     listenBroker()
-    #print(connectWifi())
 
 if __name__ == "__main__":
     main()
