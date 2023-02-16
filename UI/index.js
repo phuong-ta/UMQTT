900a945ae5098484d3899b33df96fc3e34321d09
phuong.ta <phuong.ta011290@gmail.com>
2023-02-16 11:54:47 +0200

    UI createed

--- UI/index.js
+++ UI/index.js
@@ -1,26 +1,49 @@
+const express = require('express')
 const mqtt = require('mqtt')
+const path = require('path');
+const { json } = require('express');
+const bodyParser = require('body-parser');
+const util = require('util');
+const fs = require('fs');
+const app = express()
+const port = 3000
 
-var options = {
-    host: '<your cloud>',
+const readFile = util.promisify(fs.readFile);
+app.use(bodyParser.json());
+function read(filePath = './devices.json') {
+    return readFile(path.resolve(__dirname, filePath)).then(data => JSON.parse(data));
+}
+const options = {
+    host: 'bec3bbc6c9c44b4fae7e5a42781338c5.s2.eu.hivemq.cloud',
     port: 8883,
     protocol: 'mqtts',
-    username: '<your user name>',
-    password: '<your password>'
+    username: 'tester',
+    password: '12345678'
 }
-var client = mqtt.connect(options); 
-
-// connect, then publish message to broker with LED info
+const client = mqtt.connect(options);
+var topic = "Phuong/LED";
 client.on('connect', function () {
-  client.subscribe('Phuong/LED', function (err) {
-    if (!err) {
-      client.publish('Phuong/LED', 'D3;40%') // 
-    }
-  })
-})
+    console.log("connected!");
+    client.subscribe(topic);
+});
 
-// check that did message send sucessfully
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/devices', async (req, res) => {
+    const devices = await read();
+    res.json(devices);
+});
+
+app.post('/',function(req,res) {
+    const topic = req.body.topic;
+    const msg = req.body.msg;
+    //client.publish(topic, msg);
+    console.log(topic + " value:"+ msg);
+    client.publish(topic, msg);
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
